dc9bb2d51029066690d0cde374570ee5a038f560
Johannes Schilling <jsh.schilling@fau.de>
2020-12-06 19:14:49 +0100

    environment fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-
+  firebaseConfig : {
+    apiKey: "AIzaSyByxWpx3z_OEGL4PwziJZ0BXSZnlR27M7w",
+    authDomain: "mpsuegruppe6.firebaseapp.com",
+    databaseURL: "https://mpsuegruppe6-default-rtdb.firebaseio.com",
+    projectId: "mpsuegruppe6",
+    storageBucket: "mpsuegruppe6.appspot.com",
+    messagingSenderId: "708569008212",
+    appId: "1:708569008212:web:b0d5710f7053956e5e05c1",
+    measurementId: "G-PG5CXKJNK2"
+  }
 };
+
