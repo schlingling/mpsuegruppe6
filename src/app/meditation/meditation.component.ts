b00648a18cd64780df223979e1c3052b8a26e530
Johannes Schilling <jsh.schilling@fau.de>
2021-01-14 18:37:30 +0100

    yt added

--- src/app/meditation/meditation.component.ts
+++ src/app/meditation/meditation.component.ts
@@ -5,16 +5,15 @@ import { Statement } from './../shared/interfaces/statement';
 import { Note } from './../shared/interfaces/note';
 import { DocumentService } from '../shared/document.service';
 import { Router } from '@angular/router';
-import { AuthService} from './../auth/auth.service';
-
+import { AuthService } from './../auth/auth.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-meditation',
   templateUrl: './meditation.component.html',
-  styleUrls: ['./meditation.component.css']
+  styleUrls: ['./meditation.component.css'],
 })
 export class MeditationComponent implements OnInit {
-
   timeLeft: number = 60;
   interval;
 
@@ -28,72 +27,75 @@ export class MeditationComponent implements OnInit {
 
   public text_to_save: Note = {};
 
+  safeURL: SafeResourceUrl;
+  videoURL: string = 'https://www.youtube.com/embed/s7jXASBWwwI?autoplay=1&cc_load_policy=1';
 
-
-  constructor(private questionsService: QuestionsService, private documentService: DocumentService, private router: Router, private auth: AuthService) { }
+  constructor(
+    private questionsService: QuestionsService,
+    private documentService: DocumentService,
+    private router: Router,
+    private auth: AuthService,
+    private _sanitizer: DomSanitizer
+  ) {
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(
+      this.videoURL
+    );
+  }
 
   ngOnInit(): void {
-
-    this.questions_categories = this.questionsService.getCategories()
-    console.log(this.questionsService.getCategories())
+    this.questions_categories = this.questionsService.getCategories();
+    console.log(this.questionsService.getCategories());
   }
 
   public pausePressed: boolean = false;
   public canPress: boolean = true;
 
   startTimer() {
-
-    if(this.canPress == true){
+    if (this.canPress == true) {
       this.canPress = false;
 
-      if(this.pausePressed == false){
-        this.getStatement()
+      if (this.pausePressed == false) {
+        this.getStatement();
       }
       this.interval = setInterval(() => {
-        if(this.timeLeft > 0) {
+        if (this.timeLeft > 0) {
           this.timeLeft--;
         } else {
-
-          this.defaultValue = "";
+          this.defaultValue = '';
           this.timeLeft = 60;
           this.pausePressed = false;
           this.canPress = true;
-          clearInterval(this.interval);        }
-      },20)
+          clearInterval(this.interval);
+        }
+      }, 20);
     }
-
-
   }
 
-
-
-  defaultValue: string = "";
+  defaultValue: string = '';
   upload() {
     //upload data to firestore
 
-    console.log("upload")
-
+    console.log('upload');
 
     this.text_to_save.note = this.defaultValue;
 
     //TODO
     this.text_to_save.uid = this.auth.userData.value.uid;
     this.text_to_save.statement = this.choosen_statement;
-    this.text_to_save.category = this.category
+    this.text_to_save.category = this.category;
 
-    this.documentService.setDocument('notes',this.text_to_save);
-    this.defaultValue = "";
+    this.documentService.setDocument('notes', this.text_to_save);
+    this.defaultValue = '';
   }
 
-  getStatement(){
-    this.all_statements = []
-    this.statements_with_category = []
+  getStatement() {
+    this.all_statements = [];
+    this.statements_with_category = [];
 
-    console.log(this.questions_categories)
+    console.log(this.questions_categories);
 
-    this.category = this.questions_categories[0]
-    this.questions_categories.splice(0,1)
-
+    this.category = this.questions_categories[0];
+    this.questions_categories.splice(0, 1);
 
     //INITIAL GET-REQEUST FOR MESSAGES
     this.documentService.getDocument('statements').subscribe((data) => {
@@ -102,37 +104,38 @@ export class MeditationComponent implements OnInit {
         return {
           uid: e.payload.doc.id,
           statement: q.statement,
-          category: q.category
+          category: q.category,
         } as Statement;
       });
 
       //pick a all statements, that matched the category
       this.all_statements.forEach((q) => {
-        console.log(q.category)
+        console.log(q.category);
         if (q.category === this.category) {
           this.statements_with_category.push(q);
         }
       });
 
-      if(this.statements_with_category.length <= 0){
+      if (this.statements_with_category.length <= 0) {
         //rufe die endcard-component auf
-        console.log("ende, call endcard component" + this.statements_with_category.length);
+        console.log(
+          'ende, call endcard component' + this.statements_with_category.length
+        );
         this.router.navigate(['/reflection']);
         return;
       }
 
       //pick a random statement of all the matching ones
-      this.index = Math.floor(Math.random() * this.statements_with_category.length);
+      this.index = Math.floor(
+        Math.random() * this.statements_with_category.length
+      );
 
-      this.choosen_statement = this.statements_with_category[this.index].statement
-      console.log(this.choosen_statement)
+      this.choosen_statement = this.statements_with_category[
+        this.index
+      ].statement;
+      console.log(this.choosen_statement);
 
       this.contentLoaded = Promise.resolve(true);
     });
-
-
-
-
   }
-
 }
